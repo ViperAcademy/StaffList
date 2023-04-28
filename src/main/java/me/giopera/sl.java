6ec1b30c56faf4a4a4d63d167d4bb44cace4f8db
giopera <perigio735@gmail.com>
2023-04-28 22:48:04 +0200

    ColorChanges

--- src/main/java/me/giopera/sl.java
+++ src/main/java/me/giopera/sl.java
@@ -32,8 +32,8 @@ public class sl extends Command {
 
     @Override
     public void execute(CommandSender commandSender, String[] strings) {
-        ComponentBuilder msgOwners = new ComponentBuilder("Owner ").color(ChatColor.RED).append("(").color(ChatColor.GRAY);
-        ComponentBuilder msgAdmins = new ComponentBuilder("Admin ").color(ChatColor.GOLD).append("(").color(ChatColor.GRAY);
+        ComponentBuilder msgOwners = new ComponentBuilder("Owner ").color(ChatColor.DARK_RED).append("(").color(ChatColor.GRAY);
+        ComponentBuilder msgAdmins = new ComponentBuilder("Admin ").color(ChatColor.RED).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgSrMods = new ComponentBuilder("Sr. Mod ").color(ChatColor.DARK_GREEN).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgMods = new ComponentBuilder("Mod ").color(ChatColor.GREEN).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgHelpers = new ComponentBuilder("Helper ").color(ChatColor.BLUE).append("(").color(ChatColor.GRAY);
@@ -69,11 +69,11 @@ public class sl extends Command {
             }
         }
 
-        msgOwners.append(Integer.toString(Owners)).color(ChatColor.WHITE).append(")  ").color(ChatColor.GRAY);
-        msgAdmins.append(Integer.toString(Admins)).color(ChatColor.WHITE).append(")  ").color(ChatColor.GRAY);
-        msgSrMods.append(Integer.toString(SrMods)).color(ChatColor.WHITE).append(")  ").color(ChatColor.GRAY);
-        msgMods.append(Integer.toString(Mods)).color(ChatColor.WHITE).append(")  ").color(ChatColor.GRAY);
-        msgHelpers.append(Integer.toString(Helpers)).color(ChatColor.WHITE).append(")  ").color(ChatColor.GRAY);
+        msgOwners.append(Integer.toString(Owners)).color(ChatColor.WHITE).append(") ").color(ChatColor.GRAY);
+        msgAdmins.append(Integer.toString(Admins)).color(ChatColor.WHITE).append(") ").color(ChatColor.GRAY);
+        msgSrMods.append(Integer.toString(SrMods)).color(ChatColor.WHITE).append(") ").color(ChatColor.GRAY);
+        msgMods.append(Integer.toString(Mods)).color(ChatColor.WHITE).append(") ").color(ChatColor.GRAY);
+        msgHelpers.append(Integer.toString(Helpers)).color(ChatColor.WHITE).append(") ").color(ChatColor.GRAY);
 
         msgOwners.append("▪ ").color(ChatColor.GRAY);
         msgAdmins.append("▪ ").color(ChatColor.GRAY);
