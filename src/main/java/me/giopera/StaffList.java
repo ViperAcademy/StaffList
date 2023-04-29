ac5b3661d848b031e2b52c23832c00b9406aae19
GioPera06 <perigio735@gmail.com>
2023-04-29 17:16:25 +0200

    Me: WHY IT ISN'T WORKING Also me when I notice that I haven't initialized the vanished list: Oh that's why

--- src/main/java/me/giopera/StaffList.java
+++ src/main/java/me/giopera/StaffList.java
@@ -4,11 +4,12 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class StaffList extends Plugin {
 
-    public static List<ProxiedPlayer> vanished;
+    public static List<ProxiedPlayer> vanished = new ArrayList<ProxiedPlayer>();
     public static void addVanishedPlayer(ProxiedPlayer p){
         vanished.add(p);
     }
@@ -17,6 +18,7 @@ public final class StaffList extends Plugin {
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new sl (this));
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new slvanish (this));
         getLogger().info("StaffList Avviato con successo");
+
     }
 
     @Override
