dd46ad9dd8b1ed5744e4634a52a83cb7b36d44aa
GioPera06 <perigio735@gmail.com>
2023-04-29 19:48:50 +0200

    Addeed a WhereIs Thinking to make it a StaffCore

---
+++ src/main/java/me/giopera/whereis.java
@@ -0,0 +1,18 @@
+package me.giopera;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.plugin.Command;
+
+public class whereis extends Command {
+    public whereis(StaffList stafflist) {
+        super("whereis", "whereis.use", "whereis");
+    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+        commandSender.sendMessage(new ComponentBuilder(strings[0] + " is in" + ProxyServer.getInstance().getPlayer(strings[0]).getServer()).color(ChatColor.GOLD).create());
+    }
+}
