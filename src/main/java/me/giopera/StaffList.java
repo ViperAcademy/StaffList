81858270da999ff917b626d58d526d54c06310f6
GioPera06 <perigio735@gmail.com>
2023-04-29 17:02:45 +0200

    First step to introduce VanishPlayer

--- src/main/java/me/giopera/StaffList.java
+++ src/main/java/me/giopera/StaffList.java
@@ -2,9 +2,16 @@ package me.giopera;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.util.List;
 
 public final class StaffList extends Plugin {
 
+    public static List<ProxiedPlayer> vanished;
+    public static void addVanishedPlayer(ProxiedPlayer p){
+        vanished.add(p);
+    }
     @Override
     public void onEnable() {
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new sl (this));
---
+++ src/main/java/me/giopera/slvanish.java
@@ -0,0 +1,35 @@
+package me.giopera;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+import me.giopera.StaffList;
+
+import static net.md_5.bungee.api.ProxyServer.getInstance;
+
+public class slvanish extends Command {
+    public slvanish(StaffList staffList) {
+        super("slvanish", "stafflist.vanish", "slvanish", "stafflistvanish", "vanishsl", "vanishstafflist");
+    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+        if (commandSender.hasPermission("stafflist.vanishothers") && strings.length > 0){
+            if(ProxyServer.getInstance().getPlayer(strings[1]) == null) {
+                ComponentBuilder err = new ComponentBuilder("Errore, il player non risulta essere online nel server proxy!").color(ChatColor.DARK_RED).bold(true);
+                commandSender.sendMessage(err.create());
+                return;
+            } else {
+                StaffList.addVanishedPlayer(ProxyServer.getInstance().getPlayer(strings[1]));
+                ComponentBuilder err = new ComponentBuilder("Hai attivato la vanish per " + strings[1]).color(ChatColor.GREEN).bold(true);
+                return;
+            }
+        }
+
+        StaffList.addVanishedPlayer((ProxiedPlayer) commandSender);
+
+    }
+}
