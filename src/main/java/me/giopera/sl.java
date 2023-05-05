bd7c2b9dfbb3cce66ff475a6d7dba2db983c293b
GioPera06 <perigio735@gmail.com>
2023-05-05 22:26:12 +0200

    I'M MAKING THE GRADIENTSSSSSSSSSSSSS

--- src/main/java/me/giopera/sl.java
+++ src/main/java/me/giopera/sl.java
@@ -39,9 +39,9 @@ public sl() {
         ComponentBuilder msgAdminsPlus = new ComponentBuilder("▪ Admin+ ").color(ChatColor.RED).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgAdmins = new ComponentBuilder("▪ Admin ").color(ChatColor.RED).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgCapoStaff = new ComponentBuilder("▪ Capo Staff ").color(ChatColor.of("#3dcbff")).append("(").color(ChatColor.GRAY);
-        ComponentBuilder msgSrMods = new ComponentBuilder("▪ Sr. Mod ").color(ChatColor.DARK_GREEN).append("(").color(ChatColor.GRAY);
-        ComponentBuilder msgModsPlus = new ComponentBuilder("▪ Mod+ ").color(ChatColor.of("#3bb33b")).append("(").color(ChatColor.GRAY);
-        ComponentBuilder msgMods = new ComponentBuilder("▪ Mod ").color(ChatColor.GREEN).append("(").color(ChatColor.GRAY);
+        ComponentBuilder msgSrMods = new ComponentBuilder("▪ Sr. Mod ").color(ChatColor.of("#3bb33b")).append("(").color(ChatColor.GRAY);
+        ComponentBuilder msgModsPlus = new ComponentBuilder("▪ Mod+ ").color(ChatColor.GREEN).append("(").color(ChatColor.GRAY);
+        ComponentBuilder msgMods = new ComponentBuilder("▪ Mod ").color(ChatColor.DARK_GREEN).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgHelpersPlus = new ComponentBuilder("▪ Helper+ ").color(ChatColor.of("#3e3ecf")).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgHelpers = new ComponentBuilder("▪ Helper ").color(ChatColor.BLUE).append("(").color(ChatColor.GRAY);
         ComponentBuilder msgCapoBuilder = new ComponentBuilder("▪ Capo Builder ").color(ChatColor.of("#7d00d1")).append("(").color(ChatColor.GRAY);
